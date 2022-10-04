84acbfa56276a1c14aff9eeea61eff45d227c97a
Adamhingoro <adam.abdulshakoor@gmail.com>
2022-10-04 16:22:17 +0500

    removed all code

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -16,10 +16,6 @@
   offline_message/1
 ]).
 
--define(PROCNAME, ?MODULE).
-
-%-include("ejabberd.hrl").
-%-include("jlib.hrl").
 -include_lib("xmpp/include/xmpp.hrl").
 -include("logger.hrl").
 -include("mod_muc_room.hrl").
@@ -54,24 +50,4 @@ mod_opt_type(_) ->
 -spec offline_message({_, message()}) -> {_, message()} | {stop, {drop, message()}}.
 offline_message({_Action, #message{} = Msg} = Acc) ->
   ?DEBUG("~n#################################### ENTERING CHAT OFFLINE MESSAGE (2) ###################################~n", []),
-  Token = gen_mod:get_module_opt(Message#message.from#jid.lserver, ?MODULE, auth_token),
-  PostUrl = gen_mod:get_module_opt(Message#message.from#jid.lserver, ?MODULE, post_url),
-  case Message#message.body of
-    [] -> ok;
-    [BodyTxt] ->
-      BodyText = BodyTxt#text.data,
-      ?DEBUG("mod_offline_post1: TOKEN: ~p ", [Token]),
-      ?DEBUG("mod_offline_post1: PostURL: ~p ", [PostUrl]),
-      ?DEBUG("mod_offline_post1: Text: ~p ", [BodyText]),
-      Sep = "&",
-      Post = [
-        "type=chat", Sep,
-        "to=", Message#message.to#jid.luser, Sep,
-        "from=", Message#message.from#jid.luser, Sep,
-        "body=", BodyText, Sep,
-        "access_token=", Token
-      ],
-      ?DEBUG("~n####################################~nSending post request to ~s with body \"~s\"", [PostUrl, Post]),
-      httpc:request(post, {binary_to_list(PostUrl), [], "application/x-www-form-urlencoded", list_to_binary(Post)}, [], [])
-  end,
   Recv.
