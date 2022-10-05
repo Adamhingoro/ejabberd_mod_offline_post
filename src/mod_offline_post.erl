bd85d9bb5d76c940148401e8daf72df9cf9f8690
Adamhingoro <adam.abdulshakoor@gmail.com>
2022-10-05 10:34:48 +0500

    added mod_doc

--- src/mod_offline_post.erl
+++ src/mod_offline_post.erl
@@ -1,12 +1,14 @@
 -module(mod_offline_post).
 -behaviour(gen_mod).
+
+
 -include_lib("xmpp/include/xmpp.hrl").
 
 %% Required by ?INFO_MSG macros
 -include("logger.hrl").
 
 %% gen_mod API callbacks
--export([start/2, stop/1, depends/2, reload/3, mod_options/1]).
+-export([start/2, stop/1, depends/2, reload/3, mod_options/1, mod_doc/0]).
 
 start(_Host, _Opts) ->
   ?INFO_MSG("Hello, ejabberd world!", []),
@@ -23,4 +25,7 @@ reload(_Host, _NewOpts, _OldOpts) ->
   ok.
 
 mod_options(_Host) ->
+  [].
+
+mod_doc() ->
   [].
